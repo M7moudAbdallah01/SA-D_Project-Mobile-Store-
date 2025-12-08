1077bb9b2030726d6fe3cceea44f5dd2799097d6
YoussifAdly <youssifmohammed490@gmail.com>
2025-12-08 12:06:04 +0200

    update / contact us _ js

--- JS/Contact_Us.js
+++ JS/Contact_Us.js
@@ -1,7 +1,7 @@
 document.getElementById('send').addEventListener('click', () => {
     const r = document.querySelectorAll('input[required], textarea[required]');
     if ([...r].every(input => input.value.trim() !== '')) {
-        alert('Message sent , Thank you for contacting us😊');
+        alert('Your message has been sent!');
     } else {
         alert('Please fill out all required fields.');
     }
